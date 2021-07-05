79924451e88a2a8639967708eda66f5275c16323
Gabriel Paul 'Cley Faye' Risterucci <cleyfaye@gmail.com>
2021-07-05 16:34:17 +0200

    Removed usage of external ESM package

--- grunt.js
+++ grunt.js
@@ -1,3 +1,3 @@
 // This file is kept as an "old" require/modules file to make it easier to use
 // with Gruntfile
-module.exports = require("esm")(module)("./lib/grunt");
+module.exports = require("./lib/grunt");
