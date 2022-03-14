5d0c4c3186de8f444ceb06c4d8e92a5690087f5e
Gabriel Paul 'Cley Faye' Risterucci <cleyfaye@gmail.com>
2022-03-14 16:59:54 +0100

    Added automatic listing of hidden body properties for logs

--- src/express/middlewares/bodylogger.ts
+++ src/express/middlewares/bodylogger.ts
@@ -9,6 +9,11 @@ export type ExtraLoggingDataFunc = (
 
 export type ExtraLoggingData = ExtraLoggingDataFunc | Record<string, unknown>;
 
+export interface ExtraLoggingOptions {
+  /** Do not expose the "hidden" body props */
+  noHiddenBodyProps?: boolean;
+}
+
 const setLogInfoInReq = (
   req: Request,
   body?: Array<string>,
@@ -24,15 +29,46 @@ const setLogInfoInReq = (
   }
 };
 
+const getPropType = (value: unknown): string => {
+  if (typeof value === "string") return `string(${value.length})`;
+  if (typeof value === "number") return "number";
+  if (typeof value === "boolean") return "boolean";
+  if (typeof value === "undefined") return "undefined";
+  if (Array.isArray(value)) return `Array(${value.length})`;
+  return "object";
+};
+
+const getHiddenBodyProps = (
+  req: Request,
+  ignoreKeys: Array<string> = [],
+): undefined | Record<string, string> => {
+  if (typeof req.body !== "object") return;
+  const body = req.body as Record<string, unknown>;
+  const res: Record<string, string> = {};
+  for (const key of Object.keys(body)) {
+    if (ignoreKeys.includes(key)) continue;
+    res[key] = getPropType(body[key]);
+  }
+  if (Object.keys(res).length === 0) return;
+  return res;
+};
+
 export const bodyLogger = (
   bodyProps?: Array<string>,
   extraLoggingData?: ExtraLoggingData,
+  options: ExtraLoggingOptions = {},
 ) => (req: Request, res: Response, next: NextFunction): void => {
   const asyncFunc = async () => {
+    const allExtraData: Record<string, unknown> = {};
+    if (!options.noHiddenBodyProps) {
+      const redactedBody = getHiddenBodyProps(req, bodyProps);
+      if (redactedBody) Object.assign(allExtraData, {redactedBody});
+    }
     const extraLogData = typeof extraLoggingData === "function"
       ? await extraLoggingData(req)
       : extraLoggingData;
-    setLogInfoInReq(req, bodyProps, extraLogData);
+    Object.assign(allExtraData, extraLogData);
+    setLogInfoInReq(req, bodyProps, allExtraData);
   };
   asyncFunc()
     .then(() => {
